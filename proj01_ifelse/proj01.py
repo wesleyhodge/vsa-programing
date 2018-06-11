906d1d7d476978934b7b706745837c673f7e6a9c
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-11 15:33:54 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -21,6 +21,12 @@ name=raw_input("what is your name?")
 #grade=int(raw_input("what is your grade?"))
 #print str(firstletter+name) + ", you will graduate from high school in " + str(13-grade) + " years."
 birth_month=int(raw_input("enter your birth month as a number"))
-birth_day=int(raw_input(""))
+birth_day=int(raw_input("what is the day of the month you were born?"))
+if birth_month >=month:
+   print str(name) + " you have " + str(birth_month-month) + " months till your birth day and"
+else: print str(name) + " you have " + str(12-(month-birth_month)) + " months till your birth day and"
+if birth_day >=day:
+    print str(birth_day-day) + " days till birth day."
+
 
 
