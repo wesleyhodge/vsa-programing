3f4f6859a7597c661b9010b63861538b4e39da1d
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-11 14:32:17 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -13,3 +13,14 @@
 
 
 # If you complete extensions, describe your extensions here!
+day=11
+month=6
+name=raw_input("what is your name?")
+#firstletter=name[0].upper()
+#name=name[1:].lower()
+#grade=int(raw_input("what is your grade?"))
+#print str(firstletter+name) + ", you will graduate from high school in " + str(13-grade) + " years."
+birth_month=int(raw_input("enter your birth month as a number"))
+birth_day=int(raw_input(""))
+
+
