d69404c4ce9b6254deabf10f0b75bfd85aba5c06
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-12 09:49:45 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -13,7 +13,7 @@
 
 
 # If you complete extensions, describe your extensions here!
-day=11
+day=12
 month=6
 name=raw_input("what is your name?")
 #firstletter=name[0].upper()
@@ -22,11 +22,20 @@ name=raw_input("what is your name?")
 #print str(firstletter+name) + ", you will graduate from high school in " + str(13-grade) + " years."
 birth_month=int(raw_input("enter your birth month as a number"))
 birth_day=int(raw_input("what is the day of the month you were born?"))
+age=int(raw_input("how old are you?"))
 if birth_month >=month:
-   print str(name) + " you have " + str(birth_month-month) + " months till your birth day and"
-else: print str(name) + " you have " + str(12-(month-birth_month)) + " months till your birth day and"
+   m=birth_month-month
+else:
+   m=12-(month-birth_month)
 if birth_day >=day:
-    print str(birth_day-day) + " days till birth day."
-
-
-
+    d=birth_day-day
+else:
+   m=m-1
+   d=31-day+birth_day
+print str(name) + ", you have " + str(m) + " months and " + str(d) + " days till your birth day."
+if age>=13 and age>=17:
+   print "you can see g, pg, pg-13, and R."
+elif age>=13:
+   print "you can see g, pg, and pg-13."
+else:
+   print "you can see g, and pg."
