7ecff5663075513ea120c5fff9cc445ecadf9c75
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-13 13:57:13 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -11,11 +11,13 @@ practice with lists
 
 #Part I
 #Take a list, say for example this one:
-
+lr=int(raw_input("give me a number below 100 and more than 0: "))
+the=[]
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for item in a:
-    if item < 5:
-        print item
+    if item < lr:
+        the.append(item)
+print the
 print "done"
 #and write a program that prints out all the elements of the list that are less than 5.
 
@@ -64,14 +66,15 @@ print "done"
 #Part IV
 #Ask the user for a string, and print out whether this string is a palindrome or not.
 e=[]
+h=[]
 ui=raw_input("enter a word")
+ui=ui.lower()
 for letter in ui:
     e.append(letter)
+    h.append(letter)
+e.reverse()
 print e
-
-
-
-
-
-
-
+if h == e:
+    print "it is a palindrome"
+elif h!=e:
+    print "it is not a palindrome"
