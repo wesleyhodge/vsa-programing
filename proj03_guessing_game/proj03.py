915e4987bbe92bb04cd4cdeb898a79b1350629ad
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-13 09:11:07 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -11,5 +11,26 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+import random
+answer = random.randint(1,9)
+loop=4
+d=4
+while loop != 0:
+    guess=raw_input("guess a number")
+    if guess=="exit":
+        loop=0
+    elif int(guess)==answer:
+        loop=0
+        print "you guessed the number"
+    elif int(guess)>answer:
+        d=d-1
+        loop=loop-1
+        print "too high"
+    elif int(guess)<answer:
+        loop=loop-1
+        d=d-1
+        print "too low"
+if d == 0:
+    print "you ran out of gueses"
 
 
