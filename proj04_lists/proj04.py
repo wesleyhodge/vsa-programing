895b7566caa29559c0a25584a2fc18b0a4d7c738
wesleyhodge <40075358+wesleyhodge@users.noreply.github.com>
2018-06-13 11:36:26 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -13,6 +13,10 @@ practice with lists
 #Take a list, say for example this one:
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for item in a:
+    if item < 5:
+        print item
+print "done"
 #and write a program that prints out all the elements of the list that are less than 5.
 
 
@@ -24,6 +28,12 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # Take two lists, say for example these two:
 b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+for place in c:
+    for spot in b:
+        if spot== place in c:
+            print spot
+            break
+print "done"
 # and write a program that creates and prints a list that contains only the elements
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
@@ -36,6 +46,11 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # Take a list, say for example this one:
 
 d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
+for area in d:
+    if area == "a":
+        area="*"
+    print area
+print "done"
 # and write a program that replaces all “a” with “*”.
 
 
@@ -48,8 +63,11 @@ d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
 
 #Part IV
 #Ask the user for a string, and print out whether this string is a palindrome or not.
-
-
+e=[]
+ui=raw_input("enter a word")
+for letter in ui:
+    e.append(letter)
+print e
 
 
 
